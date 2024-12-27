46aea8ec65834b50bdb7b28eaed22f59d8cbcb2b
Maksym <bienkowski.maksym@gmail.com>
2024-12-27 19:19:20 +0100

    add logging library

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
-#include <iostream>
+#include "spdlog/spdlog.h"
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/post.hpp>
 
-int main() { std::cout << "Hello" << std::endl; }
+int main() {
+  boost::asio::io_context io;
+
+  post(io, []() { spdlog::info("boost::asio::post called"); });
+  io.run();
+
+  spdlog::info("Boost.asio is done");
+}
