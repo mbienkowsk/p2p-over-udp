435fe17cebf0536ff522e43f1ffb00a28c64172f
Maksym <bienkowski.maksym@gmail.com>
2025-01-17 19:36:40 +0100

    rewrite message.h so that it actually makes sense

--- src/network/Message.h
+++ src/network/Message.h
@@ -12,21 +12,61 @@ public:
   /// The header of the message
   Header header;
 
-  /// The payload data
-  std::vector<std::byte> data;
-
   /// Serializes the message into a vector of bytes
-  std::vector<std::byte> serialize() const;
+  virtual std::vector<std::byte> serialize() const = 0;
 
   /// Deserializes a message from a vector of bytes
-  static Message deserialize(const std::vector<std::byte> &raw_data);
+  Message *from_bytes(const std::vector<std::byte> &raw_data);
 
-  /// Comparison operator for testing equality
-  bool operator==(const Message &rhs) const;
+  Message(Header header) : header(header) {};
+};
 
-  Message(const Header &header, std::vector<std::byte> data);
+/// Message broadcasted to announce available resources
+class ResourceAnnounceMessage : Message {
+public:
+  /// Names of available resources
+  std::vector<std::string> resourceNames;
 
-  const std::string dataAsString() const;
+  ResourceAnnounceMessage(Header header, std::vector<std::string> resourceNames)
+      : Message(header), resourceNames(resourceNames) {};
+
+  /// Serializes the message into a vector of bytes in the format:
+  /// header, resource names with null separators between the resource names
+  std::vector<std::byte> serialize() const override;
+};
+
+/// Message requesting a specific resource
+class ResourceRequestMessage : Message {
+public:
+  /// Name of the requested resource
+  std::string resource_name;
+
+  ResourceRequestMessage(Header header, const std::string &resource_name)
+      : Message(header), resource_name(resource_name) {};
+
+  /// Serializes the message into a vector of bytes in the format:
+  /// header, resource name as a contiguous sequence of bytes
+  std::vector<std::byte> serialize() const override;
+};
+
+/// Message containing a requested resource
+class ResourceDataMessage : Message {
+public:
+  /// Name of the resource
+  std::string resourceName;
+
+  /// Data of the resource
+  std::vector<std::byte> resourceData;
+
+  ResourceDataMessage(Header header, const std::string &resource_name,
+                      const std::vector<std::byte> &resource_data)
+      : Message(header), resourceName(resource_name),
+        resourceData(resource_data) {};
+
+  /// Serializes the message into a vector of bytes in the format:
+  /// header, resource name, resource data with a null separator
+  /// between the name and the data
+  std::vector<std::byte> serialize() const override;
 };
 
 #endif
