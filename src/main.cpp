fe190913e51ce74c8982cd2e4b2325a3eea36f2e
jedrzej-grabski <grabski.dev@gmail.com>
2025-01-22 15:00:29 +0100

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,32 @@
 #include <iostream>
 #include <thread>
 
+#include "cli/cli.h"
 #include "network/BroadcastSender.h"
 #include "resources/resource.h"
 #include "serialization/Utils.h"
+#include "log/log.h"
 
 #define PORT 12345
 #define BROADCAST_ADDR "172.21.255.255"
+#define LISTEN_PORT 8000
 
 int main() {
-  auto b = BroadcastSender(PORT, BROADCAST_ADDR);
-  auto broadcast_thread = b.make_worker([]() {
-    // TODO: make real function for this
-    return ResourceAnnounceMessage({"resource1", "resource2"});
-  });
 
-  broadcast_thread.join();
-  spdlog::info("Exiting main");
+    bool write_to_file = true;
+
+    setup_logger(write_to_file);
+
+    auto b = BroadcastSender(PORT, BROADCAST_ADDR);
+    auto broadcast_thread = b.make_worker([]() {
+        // TODO: make real function for this
+        return ResourceAnnounceMessage({"resource1", "resource2"});
+    });
+    
+    broadcast_thread.detach();  // run in background
+
+    runCLI();
+
+    spdlog::info("Exiting main");
+    return 0;
 }
