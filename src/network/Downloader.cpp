3b9430ca1a8e0a6fad880773784a7d295cc6ae1f
xDepcio <olek.drwal@gmail.com>
2025-01-23 15:23:32 +0100

    Added Downloader class

---
+++ src/network/Downloader.cpp
@@ -0,0 +1,73 @@
+#include "Downloader.h"
+#include <chrono>
+#include <exception>
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+std::unordered_map<std::string, std::shared_ptr<Downloader>>
+    Downloader::runningDownloads;
+std::mutex Downloader::mutex;
+
+bool Downloader::start()
+{
+    std::lock_guard<std::mutex> lock(Downloader::mutex);
+
+    if (Downloader::runningDownloads.find(msg.resource_name) !=
+        Downloader::runningDownloads.end())
+    {
+        spdlog::info("Resource {} is already being downloaded", msg.resource_name);
+        this->errorReason = "Resource is already being downloaded";
+        return false;
+    }
+
+    Downloader::runningDownloads[msg.resource_name] = shared_from_this();
+
+    std::thread worker([this]()
+                       {
+            size_t retry = 0;
+            while (!this->stopFlag)
+            {
+                if (retry >= 5)
+                {
+                    spdlog::error("Couldn't download: '{}', after {} retries. Aborting.",
+                                    this->msg.resource_name, retry);
+                    this->stopFlag = true;
+                    this->errorReason = "Failed to send message after 5 retries";
+                    return;
+                }
+                if (retry > 0)
+                {
+                    spdlog::info("Couldn't download: '{}', done {} retries.",
+                                this->msg.resource_name, retry);
+                }
+                try
+                {
+                    sender.sendMessage(msg);
+                }
+                catch (const std::exception &ex)
+                {
+                    this->errorReason = ex.what();
+                    this->stopFlag = true;
+                    spdlog::error("An error occured when downloading resource '{}': {}",
+                                    this->msg.resource_name, this->errorReason.value());
+                    return;
+                }
+                retry++;
+                if (this->stopFlag)
+                {
+                    break;
+                }
+                std::this_thread::sleep_for(std::chrono::seconds(3));
+            }
+            spdlog::trace("Exiting thread");
+            Downloader::runningDownloads.erase(msg.resource_name); });
+    worker.detach();
+
+    return true;
+}
+
+void Downloader::stop()
+{
+    stopFlag = true;
+    spdlog::trace("stop called");
+}
---
+++ src/network/Downloader.h
@@ -0,0 +1,56 @@
+#ifndef DOWNLOADER_H
+#define DOWNLOADER_H
+
+#include "UdpSender.h"
+#include <atomic>
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <optional>
+#include <set>
+#include <stdexcept>
+#include <thread>
+#include <spdlog/spdlog.h>
+
+class Downloader : public std::enable_shared_from_this<Downloader>
+{
+public:
+    static std::unordered_map<std::string, std::shared_ptr<Downloader>>
+        runningDownloads;
+    static std::mutex mutex;
+
+    static std::shared_ptr<Downloader> getRunningDownload(const std::string &resourceName)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        return runningDownloads[resourceName];
+    }
+
+    static std::shared_ptr<Downloader> create(const UdpSender &sender,
+                                              const ResourceRequestMessage &msg)
+    {
+        return std::shared_ptr<Downloader>(new Downloader(sender, msg));
+    }
+
+    ~Downloader()
+    {
+        spdlog::trace("~Downloader()");
+        stop();
+    }
+
+    std::optional<std::string> errorReason;
+
+    bool start();
+    void stop();
+
+private:
+    Downloader(const UdpSender &sender, const ResourceRequestMessage &msg)
+        : sender(sender), msg(msg), stopFlag(false) {};
+
+    const UdpSender &sender;
+    const ResourceRequestMessage &msg;
+    std::atomic_bool stopFlag;
+};
+
+#endif // DOWNLOADER_H
