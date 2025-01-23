97130c81b4745b35956bd786867c7f6857a0d4e9
xDepcio <olek.drwal@gmail.com>
2025-01-23 15:26:01 +0100

    added examples to main

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,31 @@
 #include <thread>
 
 #include "network/BroadcastSender.h"
+#include "network/Downloader.h"
+#include "network/UdpSender.h"
 #include "resources/resource.h"
 #include "serialization/Utils.h"
-#include "network/UdpSender.h"
 
 #define PORT 12345
 #define BROADCAST_ADDR "172.21.255.255"
 
-int main()
-{
+int main() {
   auto b = BroadcastSender(PORT, BROADCAST_ADDR);
-  auto broadcast_thread = b.make_worker([]()
-                                        {
+  auto broadcast_thread = b.make_worker([]() {
     // TODO: make real function for this
-    return ResourceAnnounceMessage({"resource1", "resource2"}); });
+    return ResourceAnnounceMessage({"resource1", "resource2"});
+  });
+
+  UdpSender sender("0.0.0.0", 8000);
+  sender.sendMessage(ResourceDataMessage(Header(MessageType::RESOURCE_DATA),
+                                         "resource1",
+                                         std::vector<std::byte>{std::byte(0)}));
+
+  sender.sendMessage(ResourceDataMessage(Header(MessageType::RESOURCE_DATA),
+                                         "resource2",
+                                         std::vector<std::byte>{std::byte(0)}));
 
   broadcast_thread.join();
-
-  UdpSender sender(
-      "0.0.0.0", 12345);
-
-  sender.sendMessage(ResourceAnnounceMessage({"resource1", "resource2"}));
 
   spdlog::info("Exiting main");
 }
