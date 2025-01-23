6495fd0bfcc16841774d8ad74aac5f64d12ef0f0
xDepcio <olek.drwal@gmail.com>
2025-01-23 15:24:41 +0100

    formatting

--- src/network/Downloader.h
+++ src/network/Downloader.h
@@ -10,47 +10,44 @@
 #include <memory>
 #include <optional>
 #include <set>
+#include <spdlog/spdlog.h>
 #include <stdexcept>
 #include <thread>
-#include <spdlog/spdlog.h>
 
-class Downloader : public std::enable_shared_from_this<Downloader>
-{
+class Downloader : public std::enable_shared_from_this<Downloader> {
 public:
-    static std::unordered_map<std::string, std::shared_ptr<Downloader>>
-        runningDownloads;
-    static std::mutex mutex;
+  static std::unordered_map<std::string, std::shared_ptr<Downloader>>
+      runningDownloads;
+  static std::mutex mutex;
 
-    static std::shared_ptr<Downloader> getRunningDownload(const std::string &resourceName)
-    {
-        std::lock_guard<std::mutex> lock(mutex);
-        return runningDownloads[resourceName];
-    }
+  static std::shared_ptr<Downloader>
+  getRunningDownload(const std::string &resourceName) {
+    std::lock_guard<std::mutex> lock(mutex);
+    return runningDownloads[resourceName];
+  }
 
-    static std::shared_ptr<Downloader> create(const UdpSender &sender,
-                                              const ResourceRequestMessage &msg)
-    {
-        return std::shared_ptr<Downloader>(new Downloader(sender, msg));
-    }
+  static std::shared_ptr<Downloader> create(const UdpSender &sender,
+                                            const ResourceRequestMessage &msg) {
+    return std::shared_ptr<Downloader>(new Downloader(sender, msg));
+  }
 
-    ~Downloader()
-    {
-        spdlog::trace("~Downloader()");
-        stop();
-    }
+  ~Downloader() {
+    spdlog::trace("~Downloader()");
+    stop();
+  }
 
-    std::optional<std::string> errorReason;
+  std::optional<std::string> errorReason;
 
-    bool start();
-    void stop();
+  bool start();
+  void stop();
 
 private:
-    Downloader(const UdpSender &sender, const ResourceRequestMessage &msg)
-        : sender(sender), msg(msg), stopFlag(false) {};
+  Downloader(const UdpSender &sender, const ResourceRequestMessage &msg)
+      : sender(sender), msg(msg), stopFlag(false){};
 
-    const UdpSender &sender;
-    const ResourceRequestMessage &msg;
-    std::atomic_bool stopFlag;
+  const UdpSender &sender;
+  const ResourceRequestMessage &msg;
+  std::atomic_bool stopFlag;
 };
 
 #endif // DOWNLOADER_H
